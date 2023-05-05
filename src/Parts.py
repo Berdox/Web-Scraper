f56b6c7f1b9108174d34414d451465c146a03ccf
Cody Hodge <ckdhodge@gmail.com>
2023-05-04 22:49:31 -0600

    done the parts part, now the weapons

--- src/Parts.py
+++ src/Parts.py
@@ -1,7 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 import re
-from classes import *
+from PartClasses import *
+import pandas as pd
 
 mapping = ['Head Parts',
             'Core Units',
@@ -13,15 +14,7 @@ mapping = ['Head Parts',
             'Secrets']
 
 pattern = '^Name|^Type|^Price|^Weight|^Energy\sDrain|^Armor\sPoints|^Armor\sPoint|^Shell\sDefense|^Energy\sDefense|^Computer\sType|^Map\sType|^Noise\sCanceller|^Bio\sSensor|^Radar\sFunction|^Radar\sRange|^Radar\sType|^Flavor\sText|^Energy\sOutput|^Maximum\sCharge|^Maximum\sLock|^Boost\sPower|^RedZone\sCharge|^Lock\sType|^Charge\sDrain|^Max\sWeight|^Anti-Missile\sResponse|^Anti-Missile\sAngle|^Extension\s\(Option\)\sSlots|^Speed|^Stability|^Jump\sFunction'
-heads = []
-core = []
-arm = []
-leg = []
-generator = []
-fcs = []
-boosters = []
-parts = [heads, core, arm, leg, generator, fcs, boosters]
-section = ''
+parts = [[], [], [], [], [], [], []]
 sectionNum = -1
 
 def partFinder(title, classes, sectionNum):
@@ -49,7 +42,6 @@ soup = BeautifulSoup(html_text, 'lxml')
 title = soup.find('h2', class_ = 'text-18 xs:text-20')
 while title.text != mapping[7]:
     if title.name == 'h2':
-        section = title.text
         sectionNum = 1 + sectionNum
         title = title.next_sibling
     else:
@@ -77,25 +69,29 @@ while title.text != mapping[7]:
                 title = partFinder(title, b, sectionNum)
         else:
             title = title.next_sibling
-         
-#for part in parts:
-#    for type in part:
-#       print(type.Armor_Points)
 
-with open('style.txt', 'w') as file:    
-    for part in parts:
-        for component in part:
-            file.write(component.Name + '\n')
-            
-'''while(title.name != 'hr'):
-                    if(re.search(pattern, title.text.strip())):
-                        subPart = re.findall(pattern, title.text.strip())[0]
-                        info = title.text.strip()
-                        info = info.removeprefix(subPart+':')
-                        if(subPart.find(' ')):
-                            subpart = subPart.replace(' ', '_')
-                        if(subPart.find('-')):
-                            subpart = subPart.replace('-', '_')
-                        setattr(h, subPart, info)
-                    title = title.next_sibling
-                parts[sectionNum].append(h)'''
+index = 1;
+for part in parts:
+    count = 0;
+    dic = dict()
+    for piece in part:
+        for key, value in vars(piece).items():
+            if(key.find('_')):
+                replacekey = key.replace('_', ' ')
+                if replacekey in dic.keys():
+                    dic[replacekey].append(value)
+                else:
+                    dic[replacekey] = [value]
+            else:
+                if key in dic.keys():
+                    dic[key].append(value)
+                else:
+                    dic[key] = [value]
+
+    for key in dic:
+       print(len(dic[key]))
+    print(dic)
+    df = pd.DataFrame(dic)
+    with pd.ExcelWriter("part" + str(index) + ".xlsx", engine = "openpyxl", mode = "w") as writer:
+        df.to_excel(writer, sheet_name = "{}".format(index), index = False, engine = "openpyxl")
+    index = 1 + index
